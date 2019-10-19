b49dfd34649be550e92b88fe18f4eb1e0dd2906a
Mark Christenson <markc@christech.us>
2019-10-19 16:58:55 -0700

    add additional lessons to finish API section

--- index.js
+++ index.js
@@ -5,12 +5,20 @@ const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require ('request');
+const bodyParser = require ('body-parser');
+
 const PORT = process.env.PORT || 5000;
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
 
 // API Key pk_538f33696ab0406bbc59b582e59e8609
 // create call_api function
-function call_api(finishedAPI) {
-	request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_538f33696ab0406bbc59b582e59e8609', {json:true}, (err, res, body) => {
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_538f33696ab0406bbc59b582e59e8609', {json:true}, (err, res, body) => {
 	if (err){return console.log(err);}
 	if (res.statusCode === 200){
 	// console.log(body);
@@ -25,13 +33,23 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff!";
 
-// Set Handlebar routes
+// Set Handlebar index GET routes
 app.get('/', function (req, res) {
 	call_api(function (doneAPI){
 		res.render('home', {
 		stock: doneAPI   
     });
-	});
+	}, "fb");
+});
+
+// Set Handlebar index POST routes
+app.post('/', function (req, res) {
+	call_api(function (doneAPI)
+	{	//posted_stuff = req.body.stock_ticker;
+		res.render('home', {
+		stock: doneAPI,
+		});
+	}, req.body.stock_ticker);
 });
 
 // Set Handlebar about page route
